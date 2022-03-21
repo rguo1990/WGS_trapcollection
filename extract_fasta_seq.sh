f7cba6daf5162f2903d20556ba303a7e7eb56a15
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-03-21 16:17:08 -0400

    Create extract_fasta_seq.sh

---
+++ extract_fasta_seq.sh
@@ -0,0 +1,4 @@
+## the command was used to extract fasta sequence from a genome assembly
+## an example to extract Scaffold_489 from the H.subflexa assembly
+
+extract_fasta_seq -q "Scaffold_489" -s H.sub_2000.fasta -o Scaffold_489.fa
