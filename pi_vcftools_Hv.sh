0880dd1ef2437e426920a5957746ed341906dd4a
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-04-05 18:37:04 -0400

    Update pi_vcftools_Hv.sh

--- pi_vcftools_Hv.sh
+++ pi_vcftools_Hv.sh
@@ -9,3 +9,13 @@ vcftools --chr NWSH01000897.1  --from-bp 13533 --to-bp 34538 --out Pi-Orco --sit
 ## OR11 and OR13
 vcftools --chr NWSH01001866.1  --from-bp 45142 --to-bp 49299 --out Pi-OR11 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01002174.1  --from-bp 97 --to-bp 4097 --out Pi-OR13 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
+
+## OR6, OR14-16, Orco
+vcftools --chr NWSH01000007.1  --from-bp 26260 --to-bp 30528 --out OR6 --recode --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01000007.1  --from-bp 78506 --to-bp 83663 --out OR14 --recode --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01000007.1  --from-bp 68291 --to-bp 77211 --out OR15 --recode --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01000007.1  --from-bp 58307 --to-bp 64441 --out OR16 --recode --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01000897.1  --from-bp 13533 --to-bp 34538 --out Orco --recode --vcf Filtered_Hvpopulation.recode.vcf
+## OR11 and OR13
+vcftools --chr NWSH01001866.1  --from-bp 45142 --to-bp 49299 --out OR11 --recode --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01002174.1  --from-bp 97 --to-bp 4097 --out OR13 --recode --vcf Filtered_Hvpopulation.recode.vcf
