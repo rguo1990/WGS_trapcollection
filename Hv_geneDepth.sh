ee6f79d8d3f0954efcb3e5b3218eeb62d4e9cc5c
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-04-18 14:45:38 -0400

    Update Hv_geneDepth.sh

--- Hv_geneDepth.sh
+++ Hv_geneDepth.sh
@@ -1,82 +1,30 @@
 ## the script was used to measure the mean depth of the whole genes vs. their CDS
 
 ## OR6 gene
-samtools coverage -r NWSH01000007.1:26260-30528 -b Hv_bamfiles.txt 
+samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:26260-30528 > depth/OR6_gene.depth
 ## OR6 CDS
-samtools coverage -r NWSH01000007.1:26260-26300 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:26725-26983 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:27443-27553 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:28260-28492 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:28790-29026 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:29550-29652 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:29719-29814 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:30373-30528 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:26260-26300 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:26725-26983 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:27443-27553 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:28260-28492 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:28790-29026 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:29550-29652 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:29719-29814 | samtools depth -f Hv_bamfiles.txt  -r NWSH01000007.1:30373-30528 > depth/OR6_CDS.depth 
 
 ## OR14 gene
-samtools coverage -r NWSH01000007.1:78506-83663 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:78506-83663 > depth/OR14_gene.depth
 ## OR14 CDS
-samtools coverage -r NWSH01000007.1:83472-83515 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:82882-83158 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:82202-82312 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:81610-81842 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:80866-81120 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:80475-80577 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:79723-79818 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:79566-79571 -b Hv_bamfiles.txt
-
-## OR15 gene
-samtools coverage -r NWSH01000007.1:68291-77211 -b Hv_bamfiles.txt
-## OR15 CDS
-samtools coverage -r NWSH01000007.1:68317-68360 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:69576-69852 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:70225-70335 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:71006-71238 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:74624-74875 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:75389-75491 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:76003-76098 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:76316-76471 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:77092-77139 -b Hv_bamfiles.txt
-
+samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:83472-83515 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:82882-83158 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:82202-82312 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:81610-81842 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:80866-81120 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:80475-80577 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:79723-79818 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:79566-79571 > depth/OR14_CDS.depth
 
 ## OR16 gene
-samtools coverage -r NWSH01000007.1:58307-64441 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:58307-64441 > depth/OR16_gene.depth
 ## OR16 CDS
-samtools coverage -r NWSH01000007.1:58367-58407 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:58691-58949 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:59928-60038 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:61380-61612 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:62019-62240 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:62720-62821 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000007.1:62894-62894 -b Hv_bamfiles.txt
-
-## OR11 gene
-samtools coverage -r NWSH01001866.1:45142-49299 -b Hv_bamfiles.txt
-## OR11 CDS
-samtools coverage -r NWSH01001866.1:49235-49266 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01001866.1:48784-49021 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01001866.1:48251-48358 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01001866.1:47610-48094 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01001866.1:47257-47358 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01001866.1:46203-46242 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:58367-58407 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:58691-58949 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:59928-60038 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:61380-61612 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:62019-62240 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:62720-62821 | samtools depth -f Hv_bamfiles.txt -r NWSH01000007.1:62894-62894 > depth/OR16_CDS.depth
 
 ## OR13 gene
-samtools coverage -r NWSH01002174.1:97-4097 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01002174.1:97-4097 > depth/OR13_gene.depth
 ## OR13 CDS
-samtools coverage -r NWSH01002174.1:97-709 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01002174.1:2387-2582 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01002174.1:2951-3165 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01002174.1:3370-4097 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01002174.1:97-709 | samtools depth -f Hv_bamfiles.txt -r NWSH01002174.1:2387-2582 | samtools depth -f Hv_bamfiles.txt -r NWSH01002174.1:2951-3165 | samtools depth -f Hv_bamfiles.txt -r NWSH01002174.1:3370-4097 > depth/OR13_CDS.depth
 
 ## orco gene
-samtools coverage -r NWSH01000897.1:13533-34538 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:13533-34538 > depth/orco_gene.depth
 ## orco CDS
-samtools coverage -r NWSH01000897.1:29259-29358 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:28112-28322 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:21276-21412 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:19216-19376 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:18769-18955 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:18130-18340 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:16132-16231 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:14799-14903 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:14241-14396 -b Hv_bamfiles.txt
-samtools coverage -r NWSH01000897.1:13665-13715 -b Hv_bamfiles.txt
+samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:29259-29358 | samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:28112-28322 | samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:21276-21412 | samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:19216-19376 | samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:18769-18955 | samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:18130-18340 
+samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:16132-16231 
+samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:14799-14903 
+samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:14241-14396 
+samtools depth -f Hv_bamfiles.txt -r NWSH01000897.1:13665-13715 
