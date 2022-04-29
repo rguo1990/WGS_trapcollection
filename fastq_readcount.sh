bc7569e3b681f2c610de449d32fc6e39970d8718
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-04-29 14:54:41 -0400

    Update fastq_readcount.sh

--- fastq_readcount.sh
+++ fastq_readcount.sh
@@ -1,3 +1,6 @@
 ## the command was used to count the number of reads of the fastq files
 
 for i in `ls *.fastq.gz`; do echo $(zcat ${i} | wc -l)/4|bc; done
+
+## the command was used to measure GC% of the fastq file
+awk '(NR%4==2) {N1+=length($0);gsub(/[AT]/,"");N2+=length($0);}END{print N2/N1;}' in.fastq
