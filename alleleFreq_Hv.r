2c5e929a3c588279faae7d47d9510f896ab47188
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-05-11 11:32:03 -0400

    Update alleleFreq_Hv.r

--- alleleFreq_Hv.r
+++ alleleFreq_Hv.r
@@ -78,6 +78,15 @@ ggplot(df,aes(positions,agct,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+
   scale_y_continuous(name="Frequency",breaks = 1:20)+labs(title = "Nonsynonymous SNP allele frequency in Hv tomatillo population")
 dev.off()
 
+## Fisher-exact test
+Group1 <- c(11,7,3,15)
+Group2 <- c(12,6,2,16)
+f <- cbind(Group1,Group2)
+fisher.test(f[1:2,])$p.value
+## p-value=1
+fisher.test(f[3:4,])$p.value
+## p-value=1
+
 ## OR14 Hv tobacco
 f <- file.path("/Users/rongguo/Downloads/HvOR14_updated", c("Hvtob1.OR14_cDNA_1.fa","Hvtob1.OR14_cDNA_2.fa", 
                                                            "Hvtob3.OR14_cDNA_1.fa", "Hvtob3.OR14_cDNA_2.fa",
@@ -123,6 +132,7 @@ colnames(df) <- c("AGCT","Freq","Pos")
 df_new <- subset(df,Freq!=18)
 ## df_new$Freq
 Frequency <- c(1,17,12,6,12,2,4,9,9,10,8,9,9,16,2,10,8,3,15,17,1,9,9,6,12,10,8,7,11,4,14,4,14,1,17,4,14,15,3,11,7,13,5,6,12)
+Group1 <- Frequency
 
 png("AF_OR14_Hvtob.png",units="in", width=16, height=8,res=300)
 ggplot(df_new,aes(Pos,Frequency,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+xlab("positions")+
@@ -173,11 +183,60 @@ colnames(df) <- c("AGCT","Freq","Pos")
 df_new <- subset(df,Freq!=18)
 ## df_new$Freq
 Frequency <- c(6,12,9,9,8,2,8,8,10,10,8,10,8,16,2,10,8,4,14,13,5,10,8,7,11,11,7,8,10,1,17,5,13,3,15,2,16,10,8,15,3,12,6,4,14)
+Group2 <- Frequency
 
 png("AF_OR14_Hvtom.png",units="in", width=16, height=8,res=300)
 ggplot(df_new,aes(Pos,Frequency,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+xlab("positions")+
   scale_y_continuous(name="Frequency",breaks = 1:20)+labs(title = "Nonsynonymous SNP allele frequency of OR14 in Hv tomatillo population")
 dev.off()
+
+## Fisher-exact test
+f <- cbind(Group1,Group2)
+fisher.test(f[1:2,])$p.value
+## p-value=0.08768328
+fisher.test(f[3:4,])$p.value
+## p-value=0.499792
+fisher.test(f[5:7,])$p.value
+## p-value=0.3574862
+fisher.test(f[8:9,])$p.value
+## p-value=1
+fisher.test(f[10:11,])$p.value
+## p-value=1
+fisher.test(f[12:13,])$p.value
+## p-value=1
+fisher.test(f[14:15,])$p.value
+## p-value=1
+fisher.test(f[16:17,])$p.value
+## p-value=1
+fisher.test(f[18:19,])$p.value
+## p-value=1
+fisher.test(f[20:21,])$p.value
+## p-value=0.1774194
+fisher.test(f[22:23,])$p.value
+## p-value=1
+fisher.test(f[24:25,])$p.value
+## p-value=1
+fisher.test(f[26:27,])$p.value
+## p-value=1
+fisher.test(f[28:29,])$p.value
+## p-value=1
+fisher.test(f[30:31,])$p.value
+## p-value=0.3376623
+fisher.test(f[32:33,])$p.value
+## p-value=1
+fisher.test(f[34:35,])$p.value
+## p-value=0.6025974
+fisher.test(f[36:37,])$p.value
+## p-value=0.6581483
+fisher.test(f[38:39,])$p.value
+## p-value=0.1463531
+fisher.test(f[40:41,])$p.value
+## p-value=0.264233
+fisher.test(f[42:43,])$p.value
+## p-value=1
+fisher.test(f[44:45,])$p.value
+## p-value=0.7111943
+
 
 ## OR16 Hv tobacco
 f <- file.path("/Users/rongguo/Downloads/HvOR16_updated", c("Hvtob1.OR16_cDNA_1.fa","Hvtob1.OR16_cDNA_2.fa", 
@@ -224,6 +283,7 @@ colnames(df) <- c("AGCT","Freq","Pos")
 df_new <- subset(df,Freq!=18)
 ## df_new$Freq
 Frequency <- c(13,5,16,2,5,13,5,13,12,6,1,17,16,2,10,8,10,8,16,2,3,15)
+Group1 <- Frequency
 
 png("AF_OR16_Hvtob.png",units="in", width=14, height=8,res=300)
 ggplot(df_new,aes(Pos,Frequency,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+xlab("positions")+
@@ -274,11 +334,37 @@ colnames(df) <- c("AGCT","Freq","Pos")
 df_new <- subset(df,Freq!=18)
 ## df_new$Freq
 Frequency <- c(10,8,15,3,3,15,3,15,10,8,4,14,13,5,12,6,12,6,13,5,1,17)
+Group2 <- Frequency
 
 png("AF_OR16_Hvtom.png",units="in", width=14, height=8,res=300)
 ggplot(df_new,aes(Pos,Frequency,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+xlab("positions")+
   scale_y_continuous(name="Frequency",breaks = 1:20)+labs(title = "Nonsynonymous SNP allele frequency of OR16 in Hv tomatillo population")
 dev.off()
+
+## Fisher-exact test
+f <- cbind(Group1,Group2)
+fisher.test(f[1:2,])$p.value
+## p-value=0.4886763
+fisher.test(f[3:4,])$p.value
+## p-value=1
+fisher.test(f[5:6,])$p.value
+## p-value=0.6905653
+fisher.test(f[7:8,])$p.value
+## p-value=0.6905653
+fisher.test(f[9:10,])$p.value
+## p-value=0.7332224
+fisher.test(f[11:12,])$p.value
+## p-value=0.3376623
+fisher.test(f[13:14,])$p.value
+## p-value=0.4017595
+fisher.test(f[15:16,])$p.value
+## p-value=0.7332224
+fisher.test(f[17:18,])$p.value
+## p-value=0.7332224
+fisher.test(f[19:20,])$p.value
+## p-value=0.4017595
+fisher.test(f[21:22,])$p.value
+## p-value=0.6025974
 
 ## OR13 Hv tobacco
 f <- file.path("/Users/rongguo/Downloads/HvOR13_updated", c("Hvtob1.OR13_cDNA_1.fa","Hvtob1.OR13_cDNA_2.fa", 
@@ -325,6 +411,7 @@ colnames(df) <- c("AGCT","Freq","Pos")
 df_new <- subset(df,Freq!=18)
 ## df_new$Freq
 Frequency <- c(9,9,6,12,6,12,11,7,10,8,11,7)
+Group1 <- Frequency
 
 png("AF_OR13_Hvtob.png",units="in", width=8, height=8,res=300)
 ggplot(df_new,aes(Pos,Frequency,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+xlab("positions")+
@@ -375,9 +462,25 @@ colnames(df) <- c("AGCT","Freq","Pos")
 df_new <- subset(df,Freq!=18)
 ## df_new$Freq
 Frequency <- c(5,13,4,14,4,14,15,3,4,14,10,8)
+Group2 <- Frequency
 
 png("AF_OR13_Hvtom.png",units="in", width=8, height=8,res=300)
 ggplot(df_new,aes(Pos,Frequency,fill=AGCT))+geom_bar(stat="identity",width = 0.5)+xlab("positions")+
   scale_y_continuous(name="Frequency",breaks = 1:20)+labs(title = "Nonsynonymous SNP allele frequency of OR13 in Hv tomatillo population")
 dev.off()
+
+## Fisher-exact test
+f <- cbind(Group1,Group2)
+fisher.test(f[1:2,])$p.value
+## p-value=0.3052667
+fisher.test(f[3:4,])$p.value
+## p-value=0.7111943
+fisher.test(f[5:6,])$p.value
+## p-value=0.7111943
+fisher.test(f[7:8,])$p.value
+## p-value=0.264233
+fisher.test(f[9:10,])$p.value
+## p-value=0.08580226
+fisher.test(f[11:12,])$p.value
+## p-value=1
 
