2aec25921d97ec105bdd27aaf22b2b0746838723
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-05-19 23:36:37 -0400

    Update pi_vcftools_Hv.sh

--- pi_vcftools_Hv.sh
+++ pi_vcftools_Hv.sh
@@ -1,7 +1,7 @@
 ## the script was used to calculate site-pi values for OR genes in Hv population
 
 ## calculate pi values for OR genes (OR6, OR14-16, Orco)
-vcftools --chr NWSH01000007.1  --from-bp 26260 --to-bp 30528 --out Pi-OR6 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01000007.1  --from-bp 26260 --to-bp 30918 --out Pi-OR6 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01000007.1  --from-bp 78506 --to-bp 83663 --out Pi-OR14 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01000007.1  --from-bp 68291 --to-bp 77211 --out Pi-OR15 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01000007.1  --from-bp 58307 --to-bp 64441 --out Pi-OR16 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
@@ -11,7 +11,7 @@ vcftools --chr NWSH01001866.1  --from-bp 45142 --to-bp 49299 --out Pi-OR11 --sit
 vcftools --chr NWSH01002174.1  --from-bp 97 --to-bp 4097 --out Pi-OR13 --site-pi --vcf Filtered_Hvpopulation.recode.vcf
 
 ## OR6, OR14-16, Orco
-vcftools --chr NWSH01000007.1  --from-bp 26260 --to-bp 30528 --out OR6 --recode --vcf Filtered_Hvpopulation.recode.vcf
+vcftools --chr NWSH01000007.1  --from-bp 26260 --to-bp 30918 --out OR6 --recode --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01000007.1  --from-bp 78506 --to-bp 83663 --out OR14 --recode --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01000007.1  --from-bp 68291 --to-bp 77211 --out OR15 --recode --vcf Filtered_Hvpopulation.recode.vcf
 vcftools --chr NWSH01000007.1  --from-bp 58307 --to-bp 64441 --out OR16 --recode --vcf Filtered_Hvpopulation.recode.vcf
